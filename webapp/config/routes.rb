773fca1ec528e3d26108491a61e0507c7d5f8e83
May <calopc.canova@gmail.com>
2016-08-28 18:50:41 +0700

    fix logout

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   get '/index', to: 'users#index'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/logout',  to: 'sessions#destroy'
 end
